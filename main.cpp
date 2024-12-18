d3ec1c9a67460c1ad53cbd1dde4b5b8a301f3c7d
Alojz-s <sozanski.aleksy@gmail.com>
2024-12-18 17:27:02 +0100

    BookManager start
    Only "shell" of the code

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "BookManager.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+}

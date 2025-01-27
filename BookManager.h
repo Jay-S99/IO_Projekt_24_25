569f1bd86102bd9472f709372b4d89f478ee587f
jaklaudek <zkl4ud1usz@gmail.com>
2025-01-27 20:41:38 +0100

    Update BookManager.h

--- BookManager.h
+++ BookManager.h
@@ -6,48 +6,50 @@
 using namespace std;
 
 //Struktura ksiazki
-struct Book 
-{
-	int id;
-	string title;
-	string author;
-	bool isAvilable;
+struct Book {
+    int id;
+    string title;
+    string author;
+    bool isAvailable;
 
-	//konstruktor
-	Book(int bookID, const string& bookTitle, const string& bookAuthor, bool availability = true);
+    // Konstruktor domyślny
+    Book() : id(0), title(""), author(""), isAvailable(true) {}
 
-	//tworzenie ksiazki z wstepnie tekstu
-	static Book fromString(const string& line);
+    // Konstruktor z argumentami
+    Book(int bookID, const string& bookTitle, const string& bookAuthor, bool availability = true)
+        : id(bookID), title(bookTitle), author(bookAuthor), isAvailable(availability) {}
 
-	//Konwersja moze zeby zapisac do pliku?
-	string toString() const;
+    // Tworzenie książki z tekstu
+    static Book fromString(const string& line);
+
+    // Konwersja książki na tekst do zapisu
+    string toString() const;
 };
 
 class BookManager
 {
 private:
-	//lista
-	vector<Book> books;
-	//przypis. ID do ksiazki
-	int nextBookID;
-	//Plik z knigami
-	const string filename; 
-	//Wczyt.
-	void loadFromFile();
-	//Zapis
-	void saveToFile() const;
+    //lista
+    vector<Book> books;
+    //przypis. ID do ksiazki
+    int nextBookID;
+    //Plik z knigami
+    const string filename; 
+    //Wczyt.
+    void loadFromFile();
+    //Zapis
+    void saveToFile() const;
 public:
-	//konst
-	BookManager(const string& file);
-	//DEST
-	~BookManager();
-	//Usuwanie
-	void removeBook(int id);
-	//Wyszukiwanie
-	void searchBook(const string& keyword) const;
-	//Wy�wietlanie
-	void displayBooks() const;
-	//Zmiana dostenosci
-	void updateAvailability(int id, bool availability);
+    //konst
+    BookManager(const string& file);
+    //DEST
+    ~BookManager();
+    //Usuwanie
+    void removeBook(int id);
+    //Wyszukiwanie
+    void searchBook(const string& keyword) const;
+    //Wyświetlanie
+    void displayBooks() const;
+    //Zmiana dostenosci
+    void updateAvailability(int id, bool availability);
 };
-

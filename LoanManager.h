5f779be6cc22bf845c6a01179f2809b74dbc2aba
jaklaudek <zkl4ud1usz@gmail.com>
2025-02-03 17:44:23 +0100

    Update LoanManager.h
    usuniecie komentarzy

--- LoanManager.h
+++ LoanManager.h
@@ -10,22 +10,17 @@ using namespace std;
 
 class LoanManager {
 private:
-    unordered_map<int, int> loans; // mapa: klucz (id u¿ytkownika) -> id ksi¹¿ki
-    const string loanFilename;    // plik do zapisywania wypo¿yczeñ
+    unordered_map<int, int> loans;
+    const string loanFilename;
 
-    // Wczytywanie danych z pliku
     void loadFromFile();
-    // Zapisywanie danych do pliku
     void saveToFile() const;
 
 public:
     LoanManager(const string& file);
     ~LoanManager();
 
-    // Wypo¿yczenie ksi¹¿ki
     void borrowBook(int userId, int bookId, BookManager& bookManager, UserManager& user);
-    // Zwrot ksi¹¿ki
     void returnBook(int userId, int bookId, BookManager& bookManager, UserManager& user);
-    // Generowanie raportu
     void generateReport(const BookManager& bookManager, const UserManager& user) const;
 };

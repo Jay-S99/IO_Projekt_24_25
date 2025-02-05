1f4fe744889f10517f9ab11646a4dfe43e3b5be6
Alojz-s <sozanski.aleksy@gmail.com>
2025-02-05 16:39:28 +0100

    Update BookManager.h

--- BookManager.h
+++ BookManager.h
@@ -1,22 +1,19 @@
-#ifndef BOOKMANAGER_H
-#define BOOKMANAGER_H
-
 #include "Book.h"
 #include <vector>
 #include <string>
+
+using namespace std;
 
 class BookManager {
 public:
     void loadBooks();
     void saveBooks();
     void addBook(const Book& book);
-    void removeBook(const std::string& title);
-    std::vector<Book> getAllBooks() const;
-    Book* searchBook(const std::string& title);
-    std::vector<Book>& getBooks();
+    void removeBook(const string& title);
+    vector<Book> getAllBooks() const;
+    Book* searchBook(const string& title);
+    vector<Book>& getBooks();
 
 private:
-    std::vector<Book> books;
+    vector<Book> books;
 };
-
-#endif

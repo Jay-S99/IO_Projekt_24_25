4f330b6fde34c38a28d620633cf2f5a75e7540bb
jaklaudek <zkl4ud1usz@gmail.com>
2025-02-05 16:42:57 +0100

    Update UserManager.h

---
+++ UserManager.h
@@ -0,0 +1,20 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include <vector>
+#include <string>
+#include "User.h"
+
+class UserManager {
+public:
+    void loadUsers();
+    void saveUsers();
+    void addUser(const User& user);
+    User* findUser(const std::string& username);
+    void showAllUsers();
+    
+private:
+    std::vector<User> users;
+};
+
+#endif

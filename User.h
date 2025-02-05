b204d3f85b2051367133d25ba1a0dcfc9a3b2db6
jaklaudek <zkl4ud1usz@gmail.com>
2025-02-05 17:03:59 +0100

    Update User.h

--- User.h
+++ User.h
@@ -1,3 +1,6 @@
+#ifndef USER_H
+#define USER_H
+
 #include <string>
 #include <vector>
 
@@ -6,13 +9,15 @@ using namespace std;
 class User {
 public:
     User(const string& username) : username(username) {}
-    
+
     string getUsername() const { return username; }
     void addLoan(const string& bookTitle);
     void returnBook(const string& bookTitle);
     void showLoanHistory() const;
-    
+
 private:
     string username;
     vector<string> loanHistory;
 };
+
+#endif
